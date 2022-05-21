f5fd3ca6b126ed28b1d7a1d5e87c9141e6b83c79
Johannes Larsen <mail@johslarsen.net>
2022-05-22 00:58:43 +0200

    Optimize for the native architecture (e.g. SIMD instructions)

--- build_and_run.sh
+++ build_and_run.sh
@@ -3,5 +3,5 @@ cd "$(dirname "$0")"
 
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Release -H. -Bcpp_O3/ && cmake --build cpp_O3/ -- -j$(nproc)
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=MinSizeRel -H. -Bcpp_Os/ && cmake --build cpp_Os/ -- -j$(nproc)
-cargo build --manifest-path=rust/Cargo.toml --release
+RUSTFLAGS="-C target-cpu=native" cargo build --manifest-path=rust/Cargo.toml --release
 ./run.sh cpp_*/bench* rust/target/release/bench*{copy,moved,raw,ref,static} | sort
